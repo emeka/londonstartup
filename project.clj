7f0d0be7a287ba7f1c8a9a1d66254ac9ebbe0036
Emeka Mosanya <emeka@mosanya.net>
2013-05-29 16:34:13 +0100

    Update gitignore, add heroku specific configuration to project.clj and added foreman Procfile.

--- project.clj
+++ project.clj
@@ -2,5 +2,10 @@
             :description "London Startup Directory"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]]
-            :main londonstartup.server)
-
+            :license {:name "Simplified BSD License"
+                      :url "http://en.wikipedia.org/wiki/BSD_licenses"}
+            :main londonstartup.server
+            :min-lein-version "2.0.0"
+            :plugins [[environ/environ.lein "0.2.1"]]
+            :hooks [environ.leiningen.hooks]
+            :profiles {:production {:env {:production true}}})
