f6e6e3c03baca33f5f08b824af1c69b0eda7b671
Emeka Mosanya <emeka@mosanya.net>
2013-06-05 14:58:01 +0100

    Added result type.

--- src/londonstartup/models/startup.clj
+++ src/londonstartup/models/startup.clj
@@ -5,53 +5,87 @@
   (import org.bson.types.ObjectId))
 
 
+;; Result
+(defn add-error [result key msg]
+  (let [errors (get-in result [:errors key] [])]
+    (assoc-in result [:errors key] (conj errors msg))))
+
+(defn result [value]
+  {:value value})
+
+(defn error [key msg]
+  {:errors {key [msg]}})
+
+(defn has-error? [result]
+  (contains? result :errors ))
+
+(defn errors [result]
+  (:errors result))
+
+(defn value [result]
+  (:value result))
+
+;; Validation
 (defn has-id [startup]
   (let [id (:_id startup)]
     (and id (not-empty (str id)))))
 
-(defn valid? [{:keys [website name]}]
-  (validate/rule (validate/has-value? website)
-    [:website "A startup must have a website"])
-  (validate/rule (validate/has-value? name)
-    [:name "A startup must have a name"])
-  ;(not (validate/errors? :website :name)) ;;TODO: valid? should be at view level.
-  )
+(def validation-rules
+  [[:website validate/has-value? "A startup must have a website"],
+   [:name validate/has-value? "A startup must have a name"]])
 
+(defn valid?
+  ([startup]
+    (reduce #(valid? startup %1 %2) (result startup) validation-rules))
+  ([startup result [field test msg]]
+    (if (test (field startup))
+      result
+      (add-error result field msg)
+      )))
+
+;; CRUD
 (let [collection "startups"]
   (defn total []
-    (mc/count collection))
+    (result (mc/count collection)))
 
   (defn id->startup [id]
-    (mc/find-one-as-map collection {:_id id}))
+    (result (mc/find-one-as-map collection {:_id id})))
 
   (defn website->startup [website]
-    (mc/find-one-as-map collection {:website website}))
+    (result (mc/find-one-as-map collection {:website website})))
 
   (defn website-free? [website]
-    (= 0 (mc/count collection {:website website})))
+    (result (= 0 (mc/count collection {:website website}))))
 
   (defn startups []
-    (mc/find-maps collection))
+    (result (mc/find-maps collection)))
 
   (defn add! [startup]
-    (when (valid? startup)
-      (let [oid (if (has-id startup) (:_id startup) (ObjectId.))]
-        (if (and (nil? (id->startup oid)) (website-free? (:website startup)))
-          (get (mc/insert-and-return collection (merge startup {:_id oid})) :_id )))))
+    (let [validation-result (valid? startup)]
+      (if (not (has-error? validation-result))
+        (let [oid (if (has-id startup) (:_id startup) (ObjectId.))]
+          (if (nil? (value (id->startup oid)))
+            (if (value (website-free? (:website startup)))
+              (result (get (mc/insert-and-return collection (merge startup {:_id oid})) :_id ))
+              (error :website "Website already in use."))
+            (error :startup "Startup already exists")))
+        validation-result)))
 
   (defn update! [startup]
-    (when (valid? startup)
-      (when-let [id (:_id startup)]
-        (when-let [old-startup (id->startup id)]
-          (if (or (= (:website startup) (:website old-startup)) (website-free? (:website startup)))
-            (do
-              (mc/update-by-id collection id startup)
-              id))))))
+    (let [validation-result (valid? startup)]
+      (if (not (has-error? validation-result))
+        (when-let [id (:_id startup)]
+          (when-let [old-startup (value (id->startup id))]
+            (if (or (= (:website startup) (:website old-startup)) (value (website-free? (:website startup))))
+              (do
+                (mc/update-by-id collection id startup)
+                (result id))
+              (error :website "Website already in use."))))
+        validation-result)))
+
 
   (defn remove! [id]
-    (mc/remove-by-id collection id))
+    (result (mc/remove-by-id collection id)))
 
   (defn remove-website! [website]
-    (mc/remove collection {:website website}))
-
-  )
+    (result (mc/remove collection {:website website}))))
--- test/londonstartup/models/startup_test.clj
+++ test/londonstartup/models/startup_test.clj
@@ -7,6 +7,36 @@
   (import org.bson.types.ObjectId))
 
 
+;; Result test
+(deftest add-error
+  (let [init-result (startup/result nil)
+        result1 (startup/add-error init-result :website "Error1")
+        result2 (startup/add-error result1 :website "Error2")
+        result3 (startup/add-error result2 :name "Name Error")]
+    (is (= {:value nil :errors {:website ["Error1"]}} result1))
+    (is (= {:value nil :errors {:website ["Error1" "Error2"]}} result2))
+    (is (= {:value nil :errors {:website ["Error1" "Error2"] :name ["Name Error"]}} result3))))
+
+(deftest result
+  (is (= {:value nil} (startup/result nil)) )
+  (is (= {:value 3} (startup/result 3))))
+
+(deftest error
+  (is (= {:errors {:website ["Error"]}} (startup/error :website "Error"))))
+
+(deftest has-error?
+  (is (not (startup/has-error? (startup/result nil))))
+  (is (startup/has-error? (startup/add-error (startup/result nil) :website "Error"))))
+
+(deftest errors
+  (is (nil? (startup/errors (startup/result nil))))
+  (is (= {:website ["Error"]} (startup/errors (startup/add-error (startup/result nil) :website "Error")))))
+
+(deftest value
+  (is (nil? (startup/value (startup/result nil))))
+  (is (= 3 (startup/value (startup/result 3)))))
+
+;; CRUD test
 (let [google-id (ObjectId.)
       yahoo-id (ObjectId.)
       github-id (ObjectId.)
@@ -14,6 +44,7 @@
       yahoo {:_id yahoo-id :website "www.yahoo.com" :name "Yahoo! Inc."}
       github {:_id github-id :website "www.github.com" :name "Github"}]
 
+;; Fixtures
   (defn init-db [f]
     (mg/connect!)
     (mg/set-db! (mg/get-db "londonstartuptest"))
@@ -28,36 +59,42 @@
   (use-fixtures :once init-db)
   (use-fixtures :each clean-db)
 
+;;Tests
+  (deftest valid?
+      (is (not (startup/has-error? (startup/valid? google))))
+      (is (startup/has-error? (startup/valid? {}))))
+
   (deftest total
-    (is (= 2 (startup/total))))
+    (is (= 2 (startup/value (startup/total)))))
 
   (deftest id->startup
-    (is (= google (startup/id->startup google-id))))
+    (is (= google (startup/value (startup/id->startup google-id)))))
 
   (deftest website->startup
-    (is (= google (startup/website->startup "www.google.com"))))
+    (is (= google (startup/value (startup/website->startup "www.google.com")))))
 
   (deftest website-free?
-    (is (startup/website-free? "www.doesnotexist.com"))
-    (is (not (startup/website-free? "www.google.com"))))
+    (is (startup/value (startup/website-free? "www.doesnotexist.com")))
+    (is (not (startup/value (startup/website-free? "www.google.com")))))
 
   (deftest startups
-    (is (= (list google yahoo) (startup/startups))))
+    (is (= (list google yahoo) (startup/value (startup/startups)))))
 
   (deftest add!
     (startup/add! github)
-    (is (= 3 (startup/total))))
+    (is (= 3 (startup/value (startup/total)))))
 
   (deftest update!
-    (is (= google-id (startup/update! (merge google {:website "www.new.com"}))))
-    (is (= "www.new.com" (get (startup/id->startup google-id) :website ))))
+    (is (= google-id (startup/value (startup/update! (merge google {:website "www.new.com"})))))
+    ;(is (= "www.new.com" (:website (startup/value (startup/id->startup google-id))))))
+    )
 
   (deftest remove!
     (startup/remove! google-id)
-    (is (= 1 (startup/total)))
-    (is (= nil (startup/id->startup google-id))))
+    (is (= 1 (startup/value (startup/total))))
+    (is (= nil (startup/value (startup/id->startup google-id)))))
 
   (deftest remove-website!
     (startup/remove-website! "www.google.com")
-    (is (= 1 (startup/total)))
-    (is (= nil (startup/id->startup google-id)))))
+    (is (= 1 (startup/value (startup/total))))
+    (is (= nil (startup/value (startup/id->startup google-id))))))
