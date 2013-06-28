91306041798272ee718d14dc02ce57228bfd7b59
Emeka Mosanya <emeka@mosanya.net>
2013-06-28 14:19:42 +0100

    Improved the form by adding boostrap helpers

--- src/londonstartup/views/bootstrap.clj
+++ src/londonstartup/views/bootstrap.clj
@@ -1,7 +1,8 @@
 (ns londonstartup.views.bootstrap
   (:use [hiccup.page :only [include-css include-js html5]]
         [hiccup.def :only [defhtml]]
-        [hiccup.element :only [link-to]]))
+        [hiccup.element :only [link-to]]
+        [hiccup.form]))
 
 
 (defn icon [name]
@@ -9,11 +10,11 @@
 
 (defn tab-headers [definitions]
   (if (not (empty? definitions))
-     (reduce #(conj %1 [(if (= 1 (count %1)) :li.active :li) [:a {:href (str "#"(:id %2)) :data-toggle "tab"} (:tab %2)]]) [:ul.nav.nav-tabs] definitions)))
+    (reduce #(conj %1 [(if (= 1 (count %1)) :li.active :li ) [:a {:href (str "#" (:id %2)) :data-toggle "tab"} (:tab %2)]]) [:ul.nav.nav-tabs ] definitions)))
 
 (defn tab-panes [definitions]
   (if (not (empty? definitions))
-     (reduce #(conj %1 [(if (= 1 (count %1)) :div.tab-pane.active :div.tab-pane) {:id (:id %2)} (:content %2)]) [:div.tab-content] definitions)))
+    (reduce #(conj %1 [(if (= 1 (count %1)) :div.tab-pane.active :div.tab-pane ) {:id (:id %2)} (:content %2)]) [:div.tab-content ] definitions)))
 
 (defn tabs [id & definitions]
   "The definitions is a list of maps.  Each map contains three elements:
@@ -27,3 +28,29 @@
      (tab-panes definitions)
      ]
     ))
+
+(defn row [& content]
+  [:div.row-fluid content])
+
+(defn span [size & content]
+  [:div {:class (str "span" size)} content])
+
+(defn span1 [& content] (span 1 content))
+(defn span2 [& content] (span 2 content))
+(defn span3 [& content] (span 3 content))
+(defn span4 [& content] (span 4 content))
+(defn span5 [& content] (span 5 content))
+(defn span6 [& content] (span 6 content))
+(defn span7 [& content] (span 7 content))
+(defn span8 [& content] (span 8 content))
+(defn span9 [& content] (span 9 content))
+(defn span10 [& content] (span 10 content))
+(defn span11 [& content] (span 11 content))
+(defn span12 [& content] (span 12 content))
+
+(defn control-group [id name & content]
+  [:div.control-group (label {:class "control-label"} id name)
+   [:div.controls content
+    ]
+   ]
+  )
