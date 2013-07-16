f0c30b335b1dc452b4da2e5efeb1032f938ea2c5
Emeka Mosanya <emeka@mosanya.net>
2013-07-16 10:22:39 +0100

    Added new MongoHQ environment variable.

--- src/londonstartup/models.clj
+++ src/londonstartup/models.clj
@@ -4,5 +4,5 @@
 
 (defn initialize []
   (let [uri (get (System/getenv) "MONGOLAB_URI"
-              (get (System/getenv) "MONGOHQ_URL" "mongodb://127.0.0.1/londonstartup"))]
+              (get (System/getenv) "MONGOHQ_URL_STARTUP_DIRECTORY" "mongodb://127.0.0.1/londonstartup"))]
     (monger.core/connect-via-uri! uri)))
