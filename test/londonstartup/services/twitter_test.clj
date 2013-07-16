4b5fb22921e4a9a57549f103565648bb7de9eaae
Emeka Mosanya <emeka@mosanya.net>
2013-07-16 09:44:33 +0100

    Remove test unitest after setting Twitter tokens in Jenkins.

--- test/londonstartup/services/twitter_test.clj
+++ test/londonstartup/services/twitter_test.clj
@@ -17,8 +17,7 @@
   (is (not (nil? t/authentication-consumer))))
 
 (deftest request-token
-  (is (not (result/has-error? (t/request-token "http://staging.startupdirectory.org/login"))))
-  (is (nil? (t/request-token "http://staging.startupdirectory.org/login"))))
+  (is (not (result/has-error? (t/request-token "http://staging.startupdirectory.org/login")))))
 
 (deftest approval-url
   (let [request-token (result/value (t/request-token "http://staging.startupdirectory.org/login"))
